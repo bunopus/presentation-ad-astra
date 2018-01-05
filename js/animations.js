61a898ad32829ceb62586746e0b472d9377f7168
Evgeny Gusev <bunopus@gmail.com>
2018-01-05 18:47:24 +0300

    Remove redundant lines

--- js/animations.js
+++ js/animations.js
@@ -1,19 +1,3 @@
-/**
- * Created by eugene.gusev on 15/12/2017.
- */
-// More info about config & dependencies:
-// - https://github.com/hakimel/reveal.js#configuration
-// - https://github.com/hakimel/reveal.js#dependencies
-Reveal.initialize({
-    history: true,
-    dependencies: [
-        { src: 'plugin/markdown/marked.js' },
-        { src: 'plugin/markdown/markdown.js' },
-        { src: 'plugin/notes/notes.js', async: true },
-        { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
-    ]
-});
-
 // Set animation delay if data-delay is specified
 Reveal.addEventListener('ready', function ( event ) {
     $('*[data-delay]').each( function () {
